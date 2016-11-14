4795ea1ca7583caa58d0c810325aeba9e045dae8
kanxinwei <shink_7770@qq.com>
2016-11-14 14:53:14 +0800

    重构

--- xsp/src/util.lua
+++ xsp/src/util.lua
@@ -6,6 +6,6 @@ function 点击(x, y, 暂停长度)
 	touchDown(index,x, y)
 	mSleep(math.random(40,60))                                --某些情况可能要增大延迟
 	touchUp(index, x, y)
-	暂停长度 = 暂停长度 or 50
+	暂停长度 = 暂停长度 or 60
 	mSleep(暂停长度)
 end
